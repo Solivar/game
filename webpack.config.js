4adca32b8f78b1af906e09fd03ab45578bcb5db9
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2018-09-07 23:49:31 +0300

    Webpack testing

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// TODO: https://webpack.js.org/guides/getting-started/#basic-setup
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
