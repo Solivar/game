983de5d42647dce7090f65ed422a3782d1e37c7a
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2018-09-27 13:09:47 +0300

    Move javascript files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './js/main.js',
+  entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.bundle.js'
