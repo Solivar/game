b82641ec3f7ca8d4f5a3997aa580b56c5aec315a
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2018-09-27 15:46:49 +0300

    Compile SASS to CSS, translate it into JS, extract it back into CSS :)

---
+++ config/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: ['./src/js/main.js', './src/scss/styles.scss'],
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'main.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+    })
+]
+};
